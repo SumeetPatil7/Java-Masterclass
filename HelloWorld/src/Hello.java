d39e7358b45c7269a122fa17f57abf45e16fb356
SumeetPatil7 <sumitpatil1299@gmail.com>
2020-12-04 13:02:28 +0530

    Main method and Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Sumeet");
+        int myFirstNumber = (10 + 5) + (2 *3);
+        int mySecondNumber = 12;
+        int myThirdNumber  = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
 
     }
 }
